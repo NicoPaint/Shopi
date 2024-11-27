58956214eda1104f2e3c691e530f44b36ae37fe4
NicoPaint <felipepinve@gmail.com>
2024-11-27 17:35:36 -0600

    I created the product's card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 //Este componente es la página principal donde se muestra todos los articulos disponibles en el Ecommerce
 //Components
 import { Layout } from "../Components/Layout"
+import { Card } from "../Components/Card"
 
 function Home() {
 
     return (
       <Layout>
         Home
+        <Card/>
       </Layout>
     )
   }
