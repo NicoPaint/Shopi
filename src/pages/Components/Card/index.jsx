ea0ca8448f08b3121bb39465db2343ddbc042a5b
NicoPaint <felipepinve@gmail.com>
2024-11-29 14:06:30 -0600

    I implemented the connection between the API and the product's cards, and applied the styles on them

--- src/pages/Components/Card/index.jsx
+++ src/pages/Components/Card/index.jsx
@@ -1,18 +1,18 @@
 //Este componente es la card donde se va a mostrar la información de los productos
 
-const Card = () => {
+const Card = ({ data }) => {
     return(
         <div className="w-56 h-60 bg-white cursor-pointer rounded-lg">
             <figure className="relative w-full h-4/5 mb-2">
-                <span className="absolute bottom-0 left-0 m-2 px-3 py-0.5 text-xs text-black bg-white/60 rounded-lg">Electronics</span>
-                <img className="w-full h-full object-cover rounded-lg" src="https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Headphones" />
+                <span className="absolute bottom-0 left-0 m-2 px-3 py-0.5 text-xs text-black bg-white/60 rounded-lg">{data.category}</span>
+                <img className="w-full h-full object-contain rounded-lg" src={data.image} alt={data.title} />
                 <div className="flex justify-center items-center absolute top-0 right-0 w-6 h-6 m-2 p-1 bg-white rounded-full">
                     +
                 </div>
             </figure>
             <p className="flex justify-between items-center mx-2">
-                <span className="text-sm font-light">Headphones</span>
-                <span className="text-lg font-medium">$200</span>
+                <span className="w-3/4 text-sm font-light truncate">{data.title}</span>
+                <span className="text-lg font-medium">${data.price}</span>
             </p>
         </div>
     )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,39 @@
 //Este componente es la página principal donde se muestra todos los articulos disponibles en el Ecommerce
+//React
+import { useState, useEffect } from "react"
 //Components
 import { Layout } from "../Components/Layout"
 import { Card } from "../Components/Card"
 
 function Home() {
+  const [products, setProducts] = useState(null)  //Este estado va a almacenar los productos que se traen de la API.
 
-    return (
-      <Layout>
-        Home
-        <Card/>
-      </Layout>
-    )
-  }
+  //Se hace el llamado a la API dentro de un useEffect para hacerlo una sola vez. Dentro esta en modo promesas y async/await
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then(response => response.json())
+      .then(data => setProducts(data));
+
+    /* const fetchProducts = async () => {
+      const response = await fetch("https://api.escuelajs.co/api/v1/products");
+      const data = await response.json();
+
+      setProducts(data)
+    }
+
+    fetchProducts(); */
+  }, [])
+
+  return (
+    <Layout>
+      Home
+      <div className="grid grid-cols-4 place-items-center w-full max-w-screen-lg gap-4">
+        {products?.map(product => (
+          <Card key={product.id} data={product}/>
+        ))}
+      </div>
+    </Layout>
+  )
+}
   
 export { Home }
