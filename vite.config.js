b54defe1897e153bcfe40ce6b8adb5e17af9f977
NicoPaint <felipepinve@gmail.com>
2025-03-26 22:06:19 -0500

    I setup the commands to deploy directly to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Shopi" /* As I'm deploying the app to GitHub Pages the base should be the repository name */
 })
